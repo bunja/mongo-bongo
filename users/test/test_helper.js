bcb1a3b6991dff4ba8bd73e73993920f916912b2
Larisa Kosareva <kos.lv@yandex.com>
2020-10-19 15:16:15 +0200

    creating a record

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
+mongoose.connect('mongodb://localhost/users_test');// initiate connect with mongodb running on our laptop
 mongoose.connection
     .once('open', () => console.log('Good to go!'))
     .on('error', (error) => {
