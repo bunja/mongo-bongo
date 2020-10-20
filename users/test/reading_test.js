77dfdfaa4649bb6d22c057d66fc1f50f0d1c6d9f
Larisa Kosareva <kos.lv@yandex.com>
2020-10-20 22:27:19 +0200

    reading test finished

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -13,8 +13,17 @@ describe('Reading users out of the database', () => {
     it('Finds all users with a name of Joe', (done) => {
         User.find({ name: 'Joe'})
             .then((users) => {
-                console.log(users);
+                console.log(users[0]._id, joe._id);
+                assert(users[0]._id.toString === joe._id.toString);
                 done();
             });
+    });
+
+    it('find a user with a particular id', (done) => {
+        User.findOne({_id: joe._id})
+            .then((user) => {
+                assert(user.name === 'Joe');
+                done();
+            })
     });
 });
