ad0d0443589e9afb4fe77c82ab8d535795f2d01c
Larisa Kosareva <kos.lv@yandex.com>
2020-10-25 20:49:28 +0100

    validation with validation function

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -6,9 +6,27 @@ describe('validating records', () => {
         const user = new User({ name: undefined });
         const validationResult = user.validateSync();
         const message = validationResult.errors.name.message;
-        //const { message } = vlidationResult.errors.name;
-        console.log("===>",message);
-
+        //const { message } = validationResult.errors.name;
+       
         assert(message === 'Name is required.')
     });
+
+    it ('requires a user\'s name longer than 2 characters', () => {
+        const user = new User ({ name: 'Al'});
+        const validationResult = user.validateSync();
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+    });
+
+    it('disallows invalid records from being saved', (done) => {
+        const user = new User({ name: 'Al'});
+        user.save()
+            .catch((validationResult) => {
+                const { message } = validationResult.errors.name;
+
+                assert(message === 'Name must be longer than 2 characters.');
+                done();
+            })
+    })
 });
