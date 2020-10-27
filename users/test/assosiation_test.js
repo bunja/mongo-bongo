da6a3862adb62d3353929dd1c633ccb8c9e47cb3
Larisa Kosareva <kos.lv@yandex.com>
2020-10-27 23:01:21 +0100

    deeply nested associations

--- users/test/assosiation_test.js
+++ users/test/assosiation_test.js
@@ -20,12 +20,35 @@ describe('Assosiations', () => {
             .then(() => done());
     });
 
-    it.only('saves a relation between a user and a blogpost', (done) => {
+    it('saves a relation between a user and a blogpost', (done) => {
         User.findOne({ name: 'Joe'})
             .populate('blogPosts')
             .then((user) => {
                 //console.log(user.blogPosts[0]);
                 assert(user.blogPosts[0].title === 'Js is Great');
+                done();
+            })
+    });
+
+    it('saves full relation graph', (done) => {
+        User.findOne({ name: 'Joe'})    //find user with name Joe
+            .populate({
+                path: 'blogPosts',      // in that user find blogPosts property and load associated blog posts
+                populate: {             //inside of these blogPosts that you just fetched 
+                    path: 'comments',    //find the comments property and load up all the associated comments
+                    model: 'comment',    //use model comment for it
+                    populate: {
+                        path: 'user',
+                        model: 'user'
+                    }
+                }
+            })
+            .then((user) => {
+                //console.log(user.blogPosts[0].comments[0]);
+                assert(user.name === 'Joe');
+                assert(user.blogPosts[0].title === 'Js is Great');
+                assert(user.blogPosts[0].comments[0].content === 'Congrats on a great post');
+                assert(user.blogPosts[0].comments[0].user.name === 'Joe');
                 done();
             })
     });
