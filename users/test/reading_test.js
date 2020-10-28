36cd286d76a897ce070b02be754915ccfd195e06
Larisa Kosareva <kos.lv@yandex.com>
2020-10-28 23:19:05 +0100

    big collections with pagination

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,13 +2,17 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-    let joe;
+    let joe, maria, alex, zach;
 
     beforeEach((done) => {
+        alex = new User({ name: 'Alex'});
+        maria = new User({ name: 'Maria'});
         joe = new User({ name: 'Joe'});
-        joe.save()
+        zach = new User({ name: 'Zach'});
+
+        Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
             .then(() => done());
-    })
+    });
 
     it('Finds all users with a name of Joe', (done) => {
         User.find({ name: 'Joe'})
@@ -25,5 +29,19 @@ describe('Reading users out of the database', () => {
                 assert(user.name === 'Joe');
                 done();
             })
+    });
+
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort({ name: 1})//sort users by the name in the ascending order
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                console.log(users);
+                assert(users.length === 2)
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
     });
 });
