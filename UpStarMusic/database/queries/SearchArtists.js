fb9850ae0e82c02b5ea43cb33ebbf974730f7fae
Larisa Kosareva <kos.lv@yandex.com>
2020-11-09 22:07:57 +0100

    search query p1

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,24 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
+    //const sortOrder = {};
+    //ortOrder[sortProperty] = 1;
+
+    const query = Artist.find({})
+            .sort({ [sortProperty]: 1})//[sortProperty] - es6 interpolated keys
+            .skip(offset)
+            .limit(limit);
+
+    return Promise.all([query, Artist.count()])
+        .then((results) => {
+            return {
+                all: results[0],
+                count: results[1],
+                offset: offset,
+                limit: limit
+            };
+        });
+
+
 };
