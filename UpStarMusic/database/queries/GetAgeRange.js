47aefcfec457b07ff2f224eeed550becbfa14bca
Larisa Kosareva <kos.lv@yandex.com>
2020-11-09 15:17:01 +0100

    queries

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+        .find({})//returns an array
+        .sort({age: 1})
+        .limit(1)// still going to be an array with a single artist inside of it 
+        .then(artists => artists[0].age);
+
+    const maxQuiry = Artist
+        .find({})
+        .sort({ age:-1})
+        .limit(1)
+        .then(artists => artists[0].age);
+
+    return Promise.all([minQuery, maxQuiry])
+        .then((result) => {
+            return { min: result[0], max: result[1]};
+        });
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+        .find({})//returns an array
+        .sort({yearsActive: 1})
+        .limit(1)// still going to be an array with a single artist inside of it 
+        .then(artists => artists[0].yearsActive);
+
+    const maxQuiry = Artist
+        .find({})
+        .sort({ yearsActive:-1})
+        .limit(1)
+        .then(artists => artists[0].yearsActive);
+
+    return Promise.all([minQuery, maxQuiry])
+        .then((result) => {
+            return { min: result[0], max: result[1]};
+        });
 };
