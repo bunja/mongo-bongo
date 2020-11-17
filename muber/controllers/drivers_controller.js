6b3389d00492616d29e1d4eb15d804685dee97c5
Larisa Kosareva <kos.lv@yandex.com>
2020-11-17 23:55:29 +0100

    edit

--- muber/controllers/drivers_controller.js
+++ muber/controllers/drivers_controller.js
@@ -11,6 +11,16 @@ module.exports = {
         Driver.create(driverProps)
             .then(driver => res.send(driver))
             .catch(next);
-        
+    },
+
+    edit(req, res, next) {
+        const driverId = req.params.id;
+        const driverProps = req.body;
+
+        Driver.findByIdAndUpdate({ _id: driverId }, driverProps)// the result of this query is just some statistics about the record, not a record
+            .then(() => Driver.findById({ _id: driverId}))
+            .then(driver => res.send(driver))
+            .catch(next);
+
     }
 };
